e4d2e51c9d81034be04499b126b67fc09c59e37b
Salman <salmanalthof31@gmail.com>
2024-11-12 22:30:33 +0700

    menghapus semua fitu test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
 console.log('cobain aja');
-
-const subtract = (a,b) => a - b;
-console.log(subtract(5,2));
-
-const multiply = (a,b) => a*b;
-console.log(multiply(5,7));
-
-const divide = (a,b) => a/b;
-console.log(divide(4,2));
