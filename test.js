56510e93754475a4be03bf60217dde43289955cc
Salman <salmanalthof31@gmail.com>
2024-11-12 22:27:15 +0700

    merevisi fungsi perkalian

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const subtract = (a,b) => a - b;
 console.log(subtract(5,2));
 
 const multiply = (a,b) => a*b;
-console.log(multiply(2,2));
+console.log(multiply(5,7));
 
 const divide = (a,b) => a/b;
 console.log(divide(4,2));
